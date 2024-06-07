f81c0411eb7b7418012de73a2523dfab187ae875
Choijina <cja224@naver.com>
2024-06-07 20:17:06 +0900

    재료, 선택 시 외곽선, 등등 추가
    코드 수정

--- scripts.js
+++ scripts.js
@@ -4,25 +4,38 @@ document.addEventListener('DOMContentLoaded', function() {
         { name: '감자', img: 'img/감자.png' },
         { name: '고구마', img: 'img/고구마.png' },
         { name: '고등어', img: 'img/고등어.png' },
+        { name: '고수', img: 'img/고수.png' },
         { name: '고추', img: 'img/고추.png' },
+        { name: '김', img: 'img/김.png' },
         { name: '김치', img: 'img/김치.png' },
+        { name: '깨', img: 'img/깨.png' },
+        { name: '깻잎', img: 'img/깻잎.png' },
         { name: '달걀', img: 'img/달걀.png' },
         { name: '당근', img: 'img/당근.png' },
         { name: '닭고기', img: 'img/닭고기.png' },
         { name: '돼지고기', img: 'img/돼지고기.png' },
         { name: '대파', img: 'img/대파.png' },
         { name: '두부', img: 'img/두부.png' },
+        { name: '떡', img: 'img/떡.png' },
+        { name: '레몬', img: 'img/레몬.png' },
         { name: '마늘', img: 'img/마늘.png' },
+        { name: '메추리알', img: 'img/메추리알.png' },
         { name: '무', img: 'img/무.png' },
         { name: '미역', img: 'img/미역.png' },
+        { name: '밥', img: 'img/밥.png' },
         { name: '배추', img: 'img/배추.png' },
         { name: '버섯', img: 'img/버섯.png' },
         { name: '베이컨', img: 'img/베이컨.png' },
+        { name: '부추', img: 'img/부추.png' },
         { name: '브로콜리', img: 'img/브로콜리.png' },
+        { name: '상추', img: 'img/상추.png' },
+        { name: '생강', img: 'img/생강.png' },
         { name: '소고기', img: 'img/소고기.png' },
         { name: '소면', img: 'img/소면.png' },
         { name: '시금치', img: 'img/시금치.png' },
         { name: '애호박', img: 'img/애호박.png' },
+        { name: '연어', img: 'img/연어.png' },
+        { name: '열무', img: 'img/열무.png' },
         { name: '양배추', img: 'img/양배추.png' },
         { name: '양상추', img: 'img/양상추.png' },
         { name: '양파', img: 'img/양파.png' },
@@ -31,20 +44,26 @@ document.addEventListener('DOMContentLoaded', function() {
         { name: '오징어', img: 'img/오징어.png' },
         { name: '우엉', img: 'img/우엉.png' },
         { name: '우유', img: 'img/우유.png' },
+        { name: '새우', img: 'img/새우.png' },
         { name: '조개', img: 'img/조개.png' },
         { name: '참치', img: 'img/참치.png' },
         { name: '치즈', img: 'img/치즈.png' },
+        { name: '카레', img: 'img/카레.png' },
         { name: '콩나물', img: 'img/콩나물.png' },
         { name: '파스타면', img: 'img/파스타면.png' },
+        { name: '파슬리', img: 'img/파슬리.png' },
         { name: '파프리카', img: 'img/파프리카.png' },
         { name: '피망', img: 'img/피망.png' },
         { name: '햄', img: 'img/햄.png' },
 
         { name: '간장', img: 'img/간장.png' },
         { name: '고추장', img: 'img/고추장.png' },
+        { name: '굴소스', img: 'img/굴소스.png' },
         { name: '꿀', img: 'img/꿀.png' },
         { name: '된장', img: 'img/된장.png' },
+        { name: '땅콩소스', img: 'img/땅콩소스.png' },
         { name: '마요네즈', img: 'img/마요네즈.png' },
+        { name: '맛술', img: 'img/맛술.png' },
         { name: '머스타드', img: 'img/머스타드.png' },
         { name: '물엿', img: 'img/물엿.png' },
         { name: '새우젓', img: 'img/새우젓.png' },
@@ -52,7 +71,10 @@ document.addEventListener('DOMContentLoaded', function() {
         { name: '소금', img: 'img/소금.png' },
         { name: '식용유', img: 'img/식용유.png' },
         { name: '식초', img: 'img/식초.png' },
+        { name: '올리고당', img: 'img/올리고당.png' },
+        { name: '올리브유', img: 'img/올리브유.png' },
         { name: '참기름', img: 'img/참기름.png' },
+        { name: '청주', img: 'img/청주.png' },
         { name: '케첩', img: 'img/케첩.png' },
         { name: '후추', img: 'img/후추.png' },
     ];
@@ -69,20 +91,27 @@ document.addEventListener('DOMContentLoaded', function() {
             img: 'img/감자.png'
         },
     ];
-
+    
     const ingredientContainer = document.getElementById('ingredients');
     const selectedIngredientContainer = document.getElementById('selected-ingredients');
     const recipeResultsContainer = document.getElementById('recipes');
 
     ingredients.forEach(ingredient => {
         const ingredientDiv = document.createElement('div');
+        ingredientDiv.style.border = '3px dotted #ffffff';
+        ingredientDiv.style.margin = '3px';
+        
         ingredientDiv.classList.add('ingredient');
         ingredientDiv.innerHTML = `
             <img src="${ingredient.img}" alt="${ingredient.name}">
             <p>${ingredient.name}</p>
         `;
-        ingredientDiv.addEventListener('click', () => selectIngredient(ingredient));
+        ingredientDiv.addEventListener('click', () => {
+            ingredientDiv.style.border = '3px dotted #ffcf10'; 
+            selectIngredient(ingredient);
+        });
         ingredientContainer.appendChild(ingredientDiv);
+        
     });
 
     const selectedIngredients = [];
@@ -133,10 +162,16 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         }, 1000);
     }
-
     function resetSelection() {
-        selectedIngredients.length = 0;
-        renderSelectedIngredients();
-        recipeResultsContainer.innerHTML = '';
-    }
+    // 모든 선택된 재료의 테두리 색상을 초기화합니다.
+    const selectedIngredientsElements = document.querySelectorAll('.ingredient');
+    selectedIngredientsElements.forEach(ingredient => {
+        ingredient.style.border = '3px dotted #ffffff';
+    });
+
+    selectedIngredients.length = 0;
+    renderSelectedIngredients();
+    recipeResultsContainer.innerHTML = '';
+}
+    
 });
